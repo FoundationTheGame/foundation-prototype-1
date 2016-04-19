3e3d1db43c9e392dd5b69e75bffabfa3ed91f478
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-04-19 14:50:55 +0100

    added camera pan controller
    simple controller for panning the camera on the xz axis

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(xAxisValue * 3, 0.0f, zAxisValue * 3));
+        
+    }
+}
