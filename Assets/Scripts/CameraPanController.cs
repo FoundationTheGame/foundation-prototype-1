f3501c2d8d822ed5a49c5a050b06e8db9b786bee
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-04-26 13:16:13 +0100

    disabled zoom control for now

---
+++ Assets/Scripts/CameraPanController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPanController : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(xAxisValue * 5, 0.0f, zAxisValue * 5));
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 200, 900), 250, Mathf.Clamp(transform.position.z, -50 , 600));
+    }
+}
---
+++ Assets/Scripts/CameraZoomController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoomController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float xAxisValue = Input.GetAxis("Horizontal");
+        /*float zAxisValue = Input.GetAxis("Zoom");
+        if (Camera.current != null)
+        {
+            
+            Camera.current.transform.Translate(new Vector3(0.0f, 0.0f, zAxisValue*5));
+            transform.parent.position = new Vector3(Mathf.Clamp(transform.parent.position.x, 210, 820), Mathf.Clamp(transform.parent.position.z, 100, 191), Mathf.Clamp(transform.parent.position.z, 170, 600));
+        }
+        */
+        
+    }
+}
