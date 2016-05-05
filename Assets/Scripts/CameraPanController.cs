a4257c28037dc4bd691364f226bb20e25b764d4f
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2016-05-05 02:34:09 +0100

    zoomed in camera
    zoomed in camera and ajusted boundaries

--- Assets/Scripts/CameraPanController.cs
+++ Assets/Scripts/CameraPanController.cs
@@ -13,6 +13,6 @@ public class CameraPanController : MonoBehaviour
         float zAxisValue = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(xAxisValue * 5, 0.0f, zAxisValue * 5));
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 200, 900), 250, Mathf.Clamp(transform.position.z, -50 , 600));
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 200, 850), 100, Mathf.Clamp(transform.position.z, 150 , 800));
     }
 }
